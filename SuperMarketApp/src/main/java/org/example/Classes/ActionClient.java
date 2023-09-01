87a705740af5512c55ccb140df6ae17158f833a3
Onna Mcadva <anna.anutka@gmail.com>
2023-09-01 16:09:29 +0200

    Added ActionClient class

--- SuperMarketApp/src/main/java/org/example/Classes/ActionClient.java
+++ SuperMarketApp/src/main/java/org/example/Classes/ActionClient.java
@@ -1,5 +1,16 @@
 package org.example.Classes;
 
+/**
+ * Класс ActionClient описывает акционного клиента. Класс наследуется от класса Actor.
+ * Поля: название акции - статическое поле, id клиента - поле класса,
+ * количество участников в акции - статическое поле.
+ * Конструктор: создает акционного клиента с именем и id.
+ * Основные методы наследуются из класс Actor.
+ * Добавлены в класс геттер и сеттер для id: getIdClient() и setIdClient().
+ * С методом isLucky() пока не могу разобраться.......
+ * @see org.example.Classes.Actor
+ */
+
 public class ActionClient extends Actor {
     public static String nameOfAction;
     private int idClient;
@@ -47,6 +58,9 @@ public class ActionClient extends Actor {
 
     public void setIdClient(int idClient) {
         this.idClient = idClient;
+    }
+    public boolean isLucky(int idClient) {
+    return this.idClient <= numberOfParticipants;
     }
 
 }

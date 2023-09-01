926e12084a1eb0fdb089e28c96aff74b080da42d
Onna Mcadva <anna.anutka@gmail.com>
2023-09-01 17:20:24 +0200

    iActorBehaviour

--- SuperMarketApp/src/main/java/org/example/Interfaces/iActorBehaviour.java
+++ SuperMarketApp/src/main/java/org/example/Interfaces/iActorBehaviour.java
@@ -5,7 +5,9 @@ import org.example.Classes.Actor;
 public interface iActorBehaviour {
     public boolean isTakeOrder();
     public boolean isMakeOrder();
+    public boolean isReturnOrder();
     public void setTakeOrder(boolean take);
     public void setMakeOrder(boolean make);
+    public void setReturnOrder(boolean returnOrder);
     Actor getActor();
 }

c9151ab4db1c349172149ead97e3fa6c5cd721e6
Onna Mcadva <anna.anutka@gmail.com>
2023-09-01 17:09:38 +0200

    OrdinaryClient

--- SuperMarketApp/src/main/java/org/example/Classes/OrdinaryClient.java
+++ SuperMarketApp/src/main/java/org/example/Classes/OrdinaryClient.java
@@ -2,27 +2,24 @@ package org.example.Classes;
 
 public class OrdinaryClient extends Actor {
 
-    // private boolean isTakenOrder;
-    // private boolean isMakeOrder;
-
     public OrdinaryClient(String name) {
         super(name);
     }
-
-    public boolean isTakeOrder() {
-        return super.isTakeOrder;
-    }
-
-    public void setTakeOrder(boolean takenOrder) {
-        super.isTakeOrder = takenOrder;
-    }
-
     public boolean isMakeOrder() {
         return super.isMakeOrder;
     }
-
     public void setMakeOrder(boolean makeOrder) {
         super.isMakeOrder = makeOrder;
+    }
+    public boolean isTakeOrder() { return super.isTakeOrder; }
+    public void setTakeOrder(boolean takenOrder) {
+        super.isTakeOrder = takenOrder;
+    }
+    public boolean isReturnOrder() {
+        return super.isReturnOrder;
+    }
+    public void setReturnOrder(boolean returnOrder) {
+        super.isReturnOrder = returnOrder;
     }
 
     public Actor getActor()

eb46a57884ecac087c8003bd0cf4cba5a9fcd137
Onna Mcadva <anna.anutka@gmail.com>
2023-09-01 21:29:05 +0200

    Creating StudentApp

--- StudentApp/src/main/java/org/example/App.java
+++ StudentApp/src/main/java/org/example/App.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.example.Domen.Student;
+import org.example.Domen.StudentGroup;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Student s1 = new Student("Иван", 25);
+        Student s2 = new Student("Игорь", 23);
+        Student s3 = new Student("Иван", 22);
+        Student s4 = new Student("Игорь",  23);
+        Student s5 = new Student("Даша",  23);
+        Student s6 = new Student("Лена",  23);
+
+        List<Student> listStud = new ArrayList<Student>();
+        listStud.add(s1);
+        listStud.add(s2);
+        listStud.add(s3);
+        listStud.add(s4);
+        listStud.add(s5);
+        listStud.add(s6);
+
+
+        StudentGroup group4580 = new StudentGroup(listStud, 4580);
+        System.out.println(group4580);
+
+        for(Student std : group4580)
+        {
+            System.out.println(std);
+        }
+
+        System.out.println("=========================================================");
+
+        Collections.sort(group4580.getGroup());
+
+        for(Student std: group4580.getGroup())
+        {
+            System.out.println(std);
+        }
     }
 }
---
+++ StudentApp/src/main/java/org/example/Domen/Teacher.java
@@ -0,0 +1,9 @@
+package org.example.Domen;
+
+public class Teacher extends Person{
+    private String acadDegree;
+    public Teacher(String firstName, int age, String acadDegree) {
+        super(firstName, age);
+        this.acadDegree = acadDegree;
+    }
+}

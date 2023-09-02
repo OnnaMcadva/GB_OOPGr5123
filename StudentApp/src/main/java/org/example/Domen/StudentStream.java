f307c80925a2bcd90457bdd7483010e0aa2d2cd2
Onna Mcadva <anna.anutka@gmail.com>
2023-09-02 16:38:43 +0200

    StudentStream

--- StudentApp/src/main/java/org/example/Domen/StudentStream.java
+++ StudentApp/src/main/java/org/example/Domen/StudentStream.java
@@ -1,5 +1,42 @@
 package org.example.Domen;
 
+import java.util.List;
+
+/**
+ * Класс StudentStream описывает поток групп студентов.
+ * Поля класса лист групп студентов и номер потока.
+ * Конструктор: создает лист групп студентов и номер потока.
+ * Добавлены в класс геттер и сеттер для номера потока (с названием потока не разобралась пока).
+ * Переопределен метод toString().
+ */
 public class StudentStream {
+    private List<StudentGroup> stream;
+    private Integer idStream;
+
+    public StudentStream(List<StudentGroup> stream, Integer idStream) {
+        this.stream = stream;
+        this.idStream = idStream;
+    }
+
+    public List<StudentGroup> getStream() {
+        return stream;
+    }
+
+    public void setStream(List<StudentGroup> stream) {
+        this.stream = stream;
+    }
+
+    public Integer getIdStream() {
+        return idStream;
+    }
+
+    public void setIdStream(Integer idStream) {
+        this.idStream = idStream;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentStream. " + "Поток № " + idStream + ": " + stream;
+    }
 
 }
